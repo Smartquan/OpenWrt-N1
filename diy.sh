57b31c4d4a914c3c1e7bbf1a7fd89a6f93f8e750
Smartquan <49008283+Smartquan@users.noreply.github.com>
2020-02-28 18:26:39 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -2,8 +2,15 @@
 #=================================================
 # Description: DIY script
 # Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
+# Author:  smart
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sudo apt-get update
+udo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev libz-dev patch python3 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex uglifyjs git-core gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx libelf-dev autoconf automake libtool autopoint device-tree-compiler
+git clone -b dev-19.07 https://github.com/Lienol/openwrt
+ cd openwrt
+./scripts/feeds clean
+./scripts/feeds update -a
+./scripts/feeds install -a
+make menuconfig
